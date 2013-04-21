49472aa46526a080bddef3d12c71cf1e4a1325af
Jacob Swartwood <jswartwood@gmail.com>
2013-04-20 23:49:30 -0400

    Quick and dirty newline separation of streams.

---
+++ index.js
@@ -0,0 +1,41 @@
+var util = require("util"),
+		stream = require("stream");
+
+var lineMatcher = /(?:\n)|(?:\r\n)|(?:\r)/;
+
+function LineStream( opts ) {
+	if (!(this instanceof LineStream))
+		return new LineStream(opts);
+
+	stream.Transform.call(this, opts);
+
+	this.newline = "\n";
+	this._temp = "";
+}
+
+//StatusStream.prototype = Object.create(stream.Transform.prototype, { constructor: { value: stream.Transform }});
+util.inherits(LineStream, stream.Transform);
+
+LineStream.prototype._transform = function _transform( chunk, encoding, done ) {
+	var lines = chunk.toString().split(lineMatcher),
+			last = lines.length - 1;
+	
+	for (var i = 0; i <= last; i++) {
+		if (i < last) {
+			this.push(this._temp + lines[i] + this.newline);
+			this._temp = "";
+		} else {
+			this._temp += lines[i];
+		}
+	}
+
+	done();
+};
+
+LineStream.prototype._flush = function _flush( done ) {
+	this.push(this._temp);
+	done();
+};
+
+module.exports = LineStream;
+
